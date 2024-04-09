c830a33f2723658ff77f71ae36bae51eafb32832
caoliu <tsaol@outlook.com>
2024-04-10 00:58:04 +0800

    update code

--- lambda_function.py
+++ lambda_function.py
@@ -2,10 +2,7 @@ import json
 import boto3
 import time
 from pricing import ANTHROPIC_PRICING
-from sns_utils import send_cost_alert
 
-COST_THRESHOLD = 100  # 设置费用阈值,单位为美元
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:342367142984:cost-alert' #SNS 主题  
 
 
 def get_token_price(model_id, token_type, region):
@@ -20,12 +17,8 @@ def lambda_handler(event, context):
     logs_client = boto3.client('logs')
 
     query = '''
-    fields input.inputTokenCount, output.outputTokenCount, @timestamp, identity.arn, modelId
-    | stats sum(input.inputTokenCount) as total_input_tokens, 
-            sum(output.outputTokenCount) as total_output_tokens,
-            count(*) as event_count
-      by bin(86400s) as day, identity.arn, modelId
-    | sort day desc, modelId
+    fields requestId, input.inputTokenCount, output.outputTokenCount, region, timestamp, identity.arn, modelId
+    | sort timestamp desc
     '''
 
     start_time = int(time.time()) - 259200  # 查询最近24小时的数据
@@ -42,42 +35,47 @@ def lambda_handler(event, context):
     response = logs_client.get_query_results(queryId=query_id)
 
     while response['status'] == 'Running':
-        time.sleep(1)
+        time.sleep(2)
         response = logs_client.get_query_results(queryId=query_id)
     
-    print('------------------------')
-    print(response)
-    print('------------------------')
-    
-    
+    print('hello')
+    print("Results:", response['results'])
+    record = ''
     for record in response['results']:
-        model_id = record[1]['value']  # 访问 'modelId' 字段
-        region = 'us-east-1'  # 替换为实际区域
-        total_input_tokens = float(record[3]['value'])  # 访问 'total_input_tokens' 字段
-        total_output_tokens = float(record[4]['value'])  # 访问 'total_output_tokens' 字段
+        model_id = None
+        region = None
+        total_input_tokens = None
+        total_output_tokens = None
+
+        for field in record:
+            if field['field'] == 'modelId':
+                model_id = field['value']
+            elif field['field'] == 'region':
+                region = field['value']
+            elif field['field'] == 'input.inputTokenCount':
+                total_input_tokens = float(field['value'])
+            elif field['field'] == 'output.outputTokenCount':
+                total_output_tokens = float(field['value'])
         
-        input_token_price = get_token_price(model_id, 'input', region)
-        output_token_price = get_token_price(model_id, 'output', region)
-        print('---record---')
+        
+        if model_id and region and total_input_tokens is not None and total_output_tokens is not None:
+            input_token_price = get_token_price(model_id, 'input', region)
+            output_token_price = get_token_price(model_id, 'output', region)
 
-        if input_token_price is not None and output_token_price is not None:
-            input_token_cost = round(total_input_tokens / 1000 * input_token_price, 2)
-            output_token_cost = round(total_output_tokens / 1000 * output_token_price, 2)
-            total_cost = round(input_token_cost + output_token_cost, 2)
-            
-            record.append({'field': 'inputTokenCost', 'value': str(input_token_cost)})
-            record.append({'field': 'outputTokenCost', 'value': str(output_token_cost)})
-            record.append({'field': 'totalCost', 'value': str(total_cost)})
-            print(record)
+            if input_token_price is not None and output_token_price is not None:
+                input_token_cost = total_input_tokens / 1000 * input_token_price
+                output_token_cost = total_output_tokens / 1000 * output_token_price
+                total_cost = input_token_cost + output_token_cost
+                record.append({'field': 'inputTokenCost', 'value': str(input_token_cost)})
+                record.append({'field': 'outputTokenCost', 'value': str(output_token_cost)})
+                record.append({'field': 'totalCost', 'value': str(total_cost)})
 
-        else:
-            record.append({'field': 'inputTokenCost', 'value': 'None'})
-            record.append({'field': 'outputTokenCost', 'value': 'None'})  
-            record.append({'field': 'totalCost', 'value': 'None'})
-            print(record)
-    
-    ##发送告警邮件 
-    send_cost_alert(total_cost, COST_THRESHOLD, SNS_TOPIC_ARN)
+            else:
+                record.append({'field': 'inputTokenCost', 'value': 'None'})
+                record.append({'field': 'outputTokenCost', 'value': 'None'})  
+                record.append({'field': 'totalCost', 'value': 'None'})
+                
+
 
     
     return response['results']
--- pricing.py
+++ pricing.py
@@ -1,15 +1,11 @@
 ANTHROPIC_PRICING = {
     "us-east-1": {
-        "claude-instant-v1": {
-            "input": 0.00080,
-            "output": 0.00240,
-        },
-        "claude-v2": {
-            "input": 0.00080,
-            "output": 0.00240,
-        },
-        "claude-v3-haiku": {"input": 0.00025, "output": 0.00125},
-        "claude-v3-sonnet": {"input": 0.00300, "output": 0.01500},
+        "claude-instant-v1": {"input": 0.00080,"output": 0.00240,},
+        "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0": {"input": 0.00080,"output": 0.00240,},
+        "claude-v2": {"input": 0.00080,"output": 0.00240,},
+        "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0": {"input": 0.003, "output": 0.015},
+        "anthropic.claude-3-sonnet-20240229-v1:0": {"input": 0.003, "output": 0.015},
+        "claude-v3-sonnet": {"input": 0.003, "output": 0.015},
     },
     "us-west-2": {
         "claude-instant-v1": {
@@ -20,7 +16,9 @@ ANTHROPIC_PRICING = {
             "input": 0.00080,
             "output": 0.00240,
         },
-        "claude-v3-sonnet": {"input": 0.00300, "output": 0.01500},
+        "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0": {"input": 0.003, "output": 0.015},
+        "anthropic.claude-3-sonnet-20240229-v1:0": {"input": 0.003, "output": 0.015},
+        "claude-v3-sonnet": {"input": 0.003, "output": 0.015},
     },
     "ap-northeast-1": {
         "claude-instant-v1": {
